a724b7200ac8e159efc2e12a96e984b6ba0c4c12
Hanlei Qin <qinhanlei@gmail.com>
2016-12-07 13:09:55 +0800

    remove old tester.

--- sorting/cpptester.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm ./*.out
-
-gcc -c common.c *_sorts.c
-ar -r libsorts.a common.o *_sorts.o
-
-g++ sorting_tester.cpp -lsorts -L. -o _cpptester.out
-
-./_cpptester.out
--- sorting/ctester.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm ./*.out
-
-gcc -c common.c *_sorts.c
-ar -r libsorts.a common.o *_sorts.o
-
-gcc sorting_tester.c -lsorts -L. -o _ctester.out
-
-./_ctester.out
