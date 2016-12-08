432ffc174e00fec749766a82dc41ea55cef70eee
Hanlei Qin <qinhanlei@gmail.com>
2016-12-08 09:00:40 +0800

    remove generic shellsort.

--- sorting/insertion_sorts.c
+++ sorting/insertion_sorts.c
@@ -66,55 +66,6 @@ void binary_insertion_sort(int arr[], int n) {
 }
 
 
-void shellsort_1(void *base, size_t num, size_t size,
-				  int (*cmp_func)(const void*, const void*))
-{
-	char *ch_base = base;
-	char *a = NULL, *b = NULL;
-	size_t i, gap;
-	int j; //note: (j -= gap) may let unsigned j overflow
-	// loop gap, let global looks orderly
-	for (gap = num / 2; gap > 0; gap /= 2) {
-		// like bubble sort below, made slow. :(
-		for (i = gap; i < num; ++i) {
-			a = ch_base + i * size; // current
-			for (j = i - gap; j >= 0; j -= gap) {
-				b = ch_base + j * size; // preceding
-				if (cmp_func(a, b) < 0) generic_swap(a, b, size);
-				a = b;
-			}
-		}
-	}
-}
-
-
-void shellsort_2(void *base, size_t num, size_t size,
-				int (*cmp_func)(const void*, const void*)) {
-	char *ch_base = base;
-	char *a = NULL, *b = NULL;
-	size_t i, gap;
-	int j; //notice: j-gap may less than zero
-	char *tmp = malloc(size);
-	if (tmp == NULL) return;
-	// loop gap, let global looks orderly
-	for (gap = num / 2; gap > 0; gap /= 2) {
-		// insertion sort below, works well. :)
-		for (i = gap; i < num; ++i) {
-			a = ch_base + i * size; // current
-			memcpy(tmp, a, size);
-			for (j = i - gap; j >= 0; j -= gap) {
-				b = ch_base + j * size;
-				if (cmp_func(tmp, b) >= 0) break;
-				memcpy(a, b, size);
-				a = b;
-			}
-			if (i != j + gap) memcpy(a, tmp, size);
-		}
-	}
-	free(tmp);
-}
-
-
 void shellsort(int arr[], int n) {
 	int i, j, gap, tmp;
 	for (gap = n/2; gap > 0; gap /= 2) {
--- sorting/insertion_sorts.h
+++ sorting/insertion_sorts.h
@@ -14,15 +14,7 @@ void insertion_sort_2(int arr[], int n);
 void binary_insertion_sort(int arr[], int n);
 
 
-// simple implement
-// first as the code at "The C++ Programming Language",
-// but it is slow (even more slow than insert sort),
-// cause too many swap operate like bubble sort.
-void shellsort_1(void *base, size_t num, size_t size,
-				  int (*cmp_func)(const void*, const void*));
-// use simple inser sort
-void shellsort_2(void *base, size_t num, size_t size,
-				int (*cmp_func)(const void*, const void*));
+
 void shellsort(int arr[], int n);
 
 
