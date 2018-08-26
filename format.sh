77c102feb27e9432d82aa7e36725b7926bd0bf01
Hanlei Qin <qinhanlei@gmail.com>
2018-08-26 23:19:10 +0800

    code fomater

---
+++ format.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if ! [ "$(which astyle)" ]; then
+    echo "Install astyle first!"
+    exit
+fi
+
+astyle \
+--mode=c \
+--indent=force-tab \
+--attach-namespaces \
+--attach-classes \
+--attach-inlines \
+--indent-col1-comments \
+--pad-header \
+--unpad-paren \
+--align-pointer=type \
+--fill-empty-lines \
+--recursive \
+--suffix=none *.c *.cpp *.h *.hpp
